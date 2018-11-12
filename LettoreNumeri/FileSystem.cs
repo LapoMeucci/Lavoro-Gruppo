35ed5f0341c1c20fb29a3cf9275bef9b31d8ce1f
Vasile-Peste <vasile.peste@protonmail.ch>
2018-11-12 09:19:54 +0100

    First commit.

---
+++ LettoreNumeri/FileSystem.cs
@@ -0,0 +1,15 @@
+﻿namespace LettoreNumeri
+{
+    public static class FileSystem
+    {
+        public static void writeFile (string path, string data)
+        {
+
+        }
+
+        public static string readFile (string path)
+        {
+            return "";
+        }
+    }
+}
